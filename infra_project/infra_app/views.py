c0a4ead0cb126917f299e1f0076f44bfd7fa16bc
Станислав Тихонов <forester@mail.ru>
2022-11-24 00:36:14 +0300

    +Django tests - 006

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!', status=200)
 
 
 def second_page(request):
